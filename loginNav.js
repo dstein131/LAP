4292bcf539dc81ab883334ccdde4b181ffe33bc7
David Stein <dstein131@gmail.com>
2022-11-22 18:08:19 -0500

    2

---
+++ loginNav.js
@@ -0,0 +1,51 @@
+function renderNavBar() {
+    const navBarRender = `
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="/images/logoclear.svg" style="height: 60px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                
+            </div>
+
+            <div class="collapse navbar-collapse  " id="navbarText">
+                <div class="ms-auto d-flex flex-row align-items-center">
+
+                    <div class="">
+                        
+
+
+
+                    </div>
+
+                    <div class="ms-3 ">
+                        <ul class="navbar-nav mb-2 mb-lg-0">
+                            <li class="nav-item">
+
+                                <a class="navbar-brand" href="#"><img src="/images/SRI Logo with Text-01.svg"
+                                        style="height: 60px"></a>
+                            </li>
+                        </ul>
+
+
+                    </div>
+                </div>
+            </div>
+    </nav>
+
+
+    `
+
+    document.getElementById("navContainer").innerHTML = navBarRender;
+
+
+
+
+
+}
+
+renderNavBar();
