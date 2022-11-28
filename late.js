782d281eda15913af92cb06eed02209867f69a74
David Stein <dstein131@gmail.com>
2022-11-28 13:22:28 -0500

    late notices

---
+++ late.js
@@ -0,0 +1,78 @@
+
+
+lateNotices = [
+    {
+        county: "Dekalb County",
+        muni: "Test",
+        dueDate: "July 31, 2019",
+        daysToFirstNotice: 30,
+        daysToLien: 60,
+        details: "Wambugu Mercy"
+    },
+    {
+        county: "Dekalb County",
+        muni: "Test",
+        dueDate: "July 31, 2019",
+        daysToFirstNotice: 30,
+        daysToLien: 60,
+        details: "Wambugu Mercy"
+    },
+    {
+        county: "Dekalb County",
+        muni: "Test",
+        dueDate: "July 31, 2019",
+        daysToFirstNotice: 30,
+        daysToLien: 60,
+        details: "Wambugu Mercy"
+    },
+    {
+        county: "Dekalb County",
+        muni: "Test",
+        dueDate: "July 31, 2019",
+        daysToFirstNotice: 30,
+        daysToLien: 60,
+        details: "Wambugu Mercy"
+    },
+    {
+        county: "Dekalb County",
+        muni: "Test",
+        dueDate: "July 31, 2019",
+        daysToFirstNotice: 30,
+        daysToLien: 60,
+        details: "Wambugu Mercy"
+    },
+    {
+        county: "Dekalb County",
+        muni: "Test",
+        dueDate: "July 31, 2019",
+        daysToFirstNotice: 30,
+        daysToLien: 60,
+        details: "Wambugu Mercy"
+    }
+]
+
+// create a function to map the array lateNoticers to the DOM element id latetable
+function lateTable(lateNotices) { 
+    var table = document.getElementById("latetable");
+    table.innerHTML = lateNotices.map(function (lateNotices) {
+        return `
+                            <tr>
+                                <td>${lateNotices.county}/${lateNotices.muni}</td>
+                                <td>Due: ${lateNotices.dueDate}</td>
+                                <td>${lateNotices.details}</td>
+                                <td>
+                                    <button type="button" class="btn btn-sm btn-primary">View</button>
+                                </td>
+                              </tr>
+        `
+    }).join("");
+}
+
+// call the function lateTable
+lateTable(lateNotices);
+
+
+
+
+
+
