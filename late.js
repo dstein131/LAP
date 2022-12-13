2e3c2962c205aa9dd789f3d08490f7b4c7b4e350
David Stein <dstein131@gmail.com>
2022-12-13 09:02:28 -0500

    badges

--- late.js
+++ late.js
@@ -7,7 +7,8 @@ lateNotices = [
         dueDate: "July 31, 2019",
         daysToFirstNotice: 30,
         daysToLien: 60,
-        details: "Wambugu Mercy"
+        details: "Wambugu Mercy",
+        noticed: true
     },
     {
         county: "Dekalb County",
@@ -15,7 +16,8 @@ lateNotices = [
         dueDate: "July 31, 2019",
         daysToFirstNotice: 30,
         daysToLien: 60,
-        details: "John Smith"
+        details: "John Smith",
+        noticed: false
     },
     {
         county: "Dekalb County",
@@ -23,7 +25,8 @@ lateNotices = [
         dueDate: "July 31, 2019",
         daysToFirstNotice: 30,
         daysToLien: 60,
-        details: "Ally Johnson"
+        details: "Ally Johnson",
+        noticed: true
     },
     {
         county: "Dekalb County",
@@ -31,7 +34,8 @@ lateNotices = [
         dueDate: "July 31, 2019",
         daysToFirstNotice: 30,
         daysToLien: 60,
-        details: "Jimmy Smith"
+        details: "Jimmy Smith",
+        noticed: true
     },
     {
         county: "Dekalb County",
@@ -39,7 +43,8 @@ lateNotices = [
         dueDate: "July 31, 2019",
         daysToFirstNotice: 30,
         daysToLien: 60,
-        details: "Allen Iverson"
+        details: "Allen Iverson",
+        noticed: false
     },
     {
         county: "Dekalb County",
@@ -47,7 +52,8 @@ lateNotices = [
         dueDate: "July 31, 2019",
         daysToFirstNotice: 30,
         daysToLien: 60,
-        details: "Steve Branch"
+        details: "Steve Branch",
+        noticed: true
     }
 ]
 
@@ -55,9 +61,10 @@ lateNotices = [
 function lateTable(lateNotices) { 
     var table = document.getElementById("latetable");
     table.innerHTML = lateNotices.map(function (lateNotices) {
+      if (lateNotices.noticed === true) {  
         return `
                             <tr>
-                                <td>${lateNotices.county}/${lateNotices.muni}</td>
+                                <td>${lateNotices.county}/${lateNotices.muni}<span class="badge bg-success ms-1">Noticed</span>
                                 <td>Due: ${lateNotices.dueDate}</td>
                                 <td>${lateNotices.details}</td>
                                 <td>
@@ -69,15 +76,38 @@ function lateTable(lateNotices) {
   <ul class="dropdown-menu">
     <li><a class="dropdown-item" href="#">Notice ${lateNotices.details}</a></li>
     <li><hr class="dropdown-divider"></li>
-    <li><a class="dropdown-item" href="#">Notice ${lateNotices.county}</a></li>
+    <li><a class="dropdown-item" href="#">Notice All Unnoticed</a></li>
   </ul>
 </div>
                                 <a href="/reports.html" ><button type="button" class="btn btn-sm btn-primary">View</button></a>
                                 </td>
                               </tr>
-        `
+        `} else {
+            return `
+            <tr>
+                <td>${lateNotices.county}/${lateNotices.muni}<span class="badge bg-danger ms-1">Unnoticed</span></td>
+                <td>Due: ${lateNotices.dueDate}</td>
+                <td>${lateNotices.details}</td>
+                <td>
+               
+<div class="btn-group">
+<button type="button" class="btn btn-sm btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+Notices
+</button>
+<ul class="dropdown-menu">
+<li><a class="dropdown-item" href="#">Notice ${lateNotices.details}</a></li>
+<li><hr class="dropdown-divider"></li>
+<li><a class="dropdown-item" href="#">Notice All Unnoticed</a></li>
+</ul>
+</div>
+                <a href="/reports.html" ><button type="button" class="btn btn-sm btn-primary">View</button></a>
+                </td>
+              </tr>
+`
+        }
     }).join("");
 }
+
 
 // call the function lateTable
 lateTable(lateNotices);
